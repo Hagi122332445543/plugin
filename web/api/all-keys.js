efb84e8f95c2f8b33199bb7b846161a251bd7caf
Hagi122332445543 <154507398+Hagi122332445543@users.noreply.github.com>
2025-11-30 12:29:00 +0300

    Add files via upload

---
+++ web/api/all-keys.js
@@ -0,0 +1,28 @@
+const LICENSE_KEYS = [
+  {
+    Key: 'DEMO262',
+    Username: 'Arsenalplayer191763',
+    endingTime: '31-12-2025'
+  },
+  {
+    Key: 'ABC123',
+    Username: '',
+    endingTime: '01-01-2026'
+  },
+  {
+    Key: 'DEF456',
+    Username: '',
+    endingTime: '15-06-2026'
+  }
+];
+
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  const adminKey = req.headers['x-admin-key'];
+  if (adminKey !== 'supersecret') {
+    return res.status(403).json({ error: 'Not Authourized To Access' });
+  }
+  res.json({ keys: LICENSE_KEYS });
+}
